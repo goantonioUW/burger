0d86187f47395550d2e06e21612dc5d38b3c8b7c
Antonio Gonzalez <goantonio31@outlook.com>
2021-01-20 14:58:13 -0800

    first commit inital setup

---
+++ directory/db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ directory/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Meeses');
+INSERT INTO burgers (burger_name) VALUES ('Bobbi');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sylvester', true);
