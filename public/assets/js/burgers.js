1c074d0c24a3ec960b718630de701a7afc0f937f
Antonio Gonzalez <goantonio31@outlook.com>
2021-02-01 18:29:13 -0800

    updated the background color and added the burgers to the top

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -24,3 +24,18 @@ $(".devouredBtn").on("click", function (e) {
         location.reload()
     })
 })
+
+$(".return").on("click", function (e) {
+    e.preventDefault()
+    let newBurger = {
+        devoured: 0
+    }
+    let id = $(this).attr("data-id")
+    $.ajax({
+        url: "/api/burgers/" + id,
+       method: "PUT",
+        data: newBurger
+    }).then(function () {
+        location.reload()
+    })
+})
