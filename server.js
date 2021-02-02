ff0b64aa336c550f4c983deb5d4f8bca0f1317b3
Antonio Gonzalez <goantonio31@outlook.com>
2021-02-02 11:15:36 -0800

    updated controller/burgers path

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-const routes = require("../burger/controllers/burgers_controller")
+const routes = require("./controllers/burgers_controller")
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
