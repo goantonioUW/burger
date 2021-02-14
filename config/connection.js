0feac8e90609afdc80af271604fe16bae8f9d74c
Antonio Gonzalez <goantonio31@outlook.com>
2021-02-14 11:44:45 -0800

    changed const to let connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
 
-const connection; 
+let  connection; 
 if (process.env.JAWSDB_URL){
 connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
